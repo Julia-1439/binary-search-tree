53e571d636c3e12854f19470ab6ee5f49dd2b2b3
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-03 13:17:39 -0800

    Import prettyPrint module

---
+++ src/prettyPrint.js
@@ -0,0 +1,21 @@
+/**
+ * Utility function to visualize a binary search tree. 
+ * @see https://www.theodinproject.com/lessons/javascript-binary-search-trees
+ */
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+export {
+  prettyPrint,
+};
