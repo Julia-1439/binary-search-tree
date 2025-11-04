3286f2640e1c6d01b31bd16a33eaee0627bc9668
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-03 17:44:32 -0800

    Add: remove method

--- src/index.js
+++ src/index.js
@@ -39,3 +39,53 @@ try {
   console.warn("Inserting duplicate element 5");
 }
 console.log(T.toString());
+
+arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 0, -1];
+T = new Tree(arr);
+console.log("Starting tree:", T.toString());
+
+// remove leaf
+T.remove(6345);
+console.log("Removed leaf node 6345:", T.toString());
+// remove node with one right child and no left child
+T.remove(0);
+console.log("Removed node 0 with one right child and no left child:", T.toString());
+// remove node with one left child and no right child
+T.remove(67);
+console.log("Removed node 67 with one left child and no right child:", T.toString());
+// remove node with two children
+T.remove(4);
+console.log("Removed node 4 with two LEAF children:", T.toString());
+T.remove(23);
+console.log("Removed node 23 with two LEAF children:", T.toString());
+// remove root (and the replacement having a left child)
+T.insert(4);
+console.log("Inserted 4:", T.toString());
+T.remove(7);
+console.log("Removed root 7 and with replacement 5 having a left child:", T.toString());
+
+// remove non-existent element
+T.remove(9999);
+console.log("Attempted remove non-existent value 9999:", T.toString());
+
+// remove everything
+T.remove(5);
+T.remove(9);
+T.remove(8);
+T.remove(1);
+T.remove(3);
+T.remove(4);
+T.remove(-1);
+T.remove(324);
+console.log("Removed everything:", T.toString());
+
+// remove from empty tree
+T.remove(5);
+T.remove(null);
+console.log("Remove from empty tree:", T.toString());
+
+// remove root from singleton tree
+T.insert(5);
+console.log("Insert element (singleton tree):", T.toString());
+T.remove(5);
+console.log("Remove element from singleton tree:", T.toString());
--- src/tree.js
+++ src/tree.js
@@ -4,31 +4,30 @@ import { prettyPrint } from "./prettyPrint.js";
 /**
  * Builds a balanced binary search tree from an array of numbers. The numbers
  * need not be pre-sorted or have all unique values, as that will be taken care
- * of internally. 
+ * of internally.
  */
 class Tree {
   constructor(arr) {
-    this.root = this.buildTree(arr);  
+    this.root = this.buildTree(arr);
   }
 
   buildTree(arr) {
-    // Initial processing: remove duplicates, then sort.   
-    arr = [...new Set(arr)]; 
+    // Initial processing: remove duplicates, then sort.
+    arr = [...new Set(arr)];
     arr.sort((a, b) => a - b);
 
     return this.#buildTreeRecur(arr, 0, arr.length - 1);
   }
 
   /**
-   * 
+   *
    * @param {Array{Number}} arr must be sorted and free of duplicates
    * @param {Number} start defines the start index of arr from which to build a tree, *inclusive*
    * @param {Number} end defines the end index of arr from which to build a tree, also *inclusive*.
    * (tried the math with `end` being exclusive - didn't work out once get down to the leaf nodes)
    */
   #buildTreeRecur(arr, start, end) {
-
-    // Stopping condition: tried to build a left or right subtree from a leaf node  
+    // Stopping condition: tried to build a left or right subtree from a leaf node
     if (end < start) {
       return null;
     }
@@ -49,28 +48,99 @@ class Tree {
     }
 
     let curr = this.root;
-    while (true) { // will terminate upon insertion or finding `value` is a duplicate
+    while (true) {
+      // will terminate upon insertion or finding `value` is a duplicate
       if (value < curr.value) {
         if (curr.left) {
           curr = curr.left;
-        } 
-        else {
+        } else {
           curr.left = new Node(value);
           break;
         }
-      }
-      else if (value > curr.value) {
-        if (curr.right) 
-          curr = curr.right;
+      } else if (value > curr.value) {
+        if (curr.right) curr = curr.right;
         else {
           curr.right = new Node(value);
           break;
         }
-      }
-      else {
+      } else {
         throw new Error("This value already exists in the tree.");
       }
     }
+  }
+
+  /**
+   *
+   * @param {Number} value
+   * @returns {Node} new tree with the node containing `value` removed, or
+   * an unchanged tree if `value` did not exist 
+   */
+  remove(value) {
+    if (this.root === null) {
+      return this.root;
+    }
+
+    this.root = this.#removeRecur(this.root, value);
+  }
+
+  /**
+   * 
+   * @param {Node} currNode 
+   * @param {Number} value 
+   * @returns {Node | null} if `currNode` contains `value`, then its replacement node. 
+   * else, `currNode`. This return value is used one level up the stack
+   * to restructure the tree as needed. 
+   */
+  #removeRecur(currNode, value) {
+    // Stopping case (1): went past a leaf node - element not found
+    if (currNode === null) {
+      return currNode;
+    }
+
+    if (value < currNode.value) {
+      currNode.left = this.#removeRecur(currNode.left, value);
+    } 
+    else if (value > currNode.value) {
+      currNode.right = this.#removeRecur(currNode.right, value);
+    }
+    // Stopping case (2): Found a match
+    else { 
+      // Case 1: at a leaf node
+      if (!currNode.left && !currNode.right) {
+        return null;
+      }
+      // Case 2a: current node has one child, a left child
+      else if (currNode.left && !currNode.right) {
+        return currNode.left;
+      }
+      // Case 2b: current node has one child, a right child
+      else if (!currNode.left && currNode.right) {
+        return currNode.right;
+      }
+      // Case 3: current node has two child nodes
+      else {
+        const replacement = this.#getInorderPredecessor(currNode);
+        currNode.value = replacement.value;
+        currNode.left = this.#removeRecur(currNode.left, replacement.value);
+        return currNode;
+      }
+    }
+
+    return currNode;
+  }
+
+  /**
+   * Utility function in removal. Note that using the inorder-successor is also
+   * permitted.
+   * @param {Node} currNode part of a BST having two children
+   * @returns {Node} the inorder-predecessor of `currNode` 
+   */
+  #getInorderPredecessor(currNode) {
+    currNode = currNode.left;
+    while (currNode.right !== null) {
+      currNode = currNode.right;
+    }
+    return currNode;
   }
 
   toString() {
@@ -78,6 +148,4 @@ class Tree {
   }
 }
 
-export {
-  Tree,
-};
+export { Tree };
